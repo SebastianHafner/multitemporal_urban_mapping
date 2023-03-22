377d62b71945759c50de9d066002151c1908f7e8
SebastianHafner <hafnersailing@gmail.com>
2023-03-22 17:17:45 +0100

    final

--- utils/parsers.py
+++ utils/parsers.py
@@ -19,6 +19,24 @@ def training_argument_parser():
     return parser
 
 
+def sweep_argument_parser():
+    # https://docs.python.org/3/library/argparse.html#the-add-argument-method
+    parser = argparse.ArgumentParser(description="Experiment Args")
+    parser.add_argument('-c', "--config-file", dest='config_file', required=True, help="path to config file")
+    parser.add_argument('-o', "--output-dir", dest='output_dir', required=True, help="path to output directory")
+    parser.add_argument('-d', "--dataset-dir", dest='dataset_dir', default="", required=True,
+                        help="path to output directory")
+    parser.add_argument('-p', "--wandb-project", dest='wandb_project', required=True, help="wandb project")
+
+    parser.add_argument(
+        "opts",
+        help="Modify config options using the command-line",
+        default=None,
+        nargs=argparse.REMAINDER,
+    )
+    return parser
+
+
 def deployement_argument_parser():
     # https://docs.python.org/3/library/argparse.html#the-add-argument-method
     parser = argparse.ArgumentParser(description="Experiment Args")
