284339dc0637d235643a869cdaf74aa1887a2ddb
SebastianHafner <hafnersailing@gmail.com>
2023-04-30 17:29:55 +0200

    maybe indent fixes everything

--- run_sweep.py
+++ run_sweep.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print('=== Runnning on device: p', device)
 
-    def run_training(sweep_cfg = None):
+    def run_training(sweep_cfg=None):
 
         with wandb.init(config=sweep_cfg):
             sweep_cfg = wandb.config
@@ -28,101 +28,101 @@ if __name__ == '__main__':
             torch.backends.cudnn.deterministic = True
             torch.backends.cudnn.benchmark = False
 
-        net = model_factory.create_network(cfg)
-        net.to(device)
-        optimizer = optim.AdamW(net.parameters(), lr=sweep_cfg.lr, weight_decay=0.01)
+            net = model_factory.create_network(cfg)
+            net.to(device)
+            optimizer = optim.AdamW(net.parameters(), lr=sweep_cfg.lr, weight_decay=0.01)
 
-        criterion = loss_factory.get_criterion(cfg.MODEL.LOSS_TYPE)
+            criterion = loss_factory.get_criterion(cfg.MODEL.LOSS_TYPE)
 
-        # reset the generators
-        dataset = datasets.TrainDataset(cfg=cfg, run_type='train')
-        print(dataset)
+            # reset the generators
+            dataset = datasets.TrainDataset(cfg=cfg, run_type='train')
+            print(dataset)
 
-        dataloader_kwargs = {
-            'batch_size': sweep_cfg.batch_size,
-            'num_workers': 0 if cfg.DEBUG else cfg.DATALOADER.NUM_WORKER,
-            'shuffle': cfg.DATALOADER.SHUFFLE,
-            'drop_last': True,
-            'pin_memory': True,
-        }
-        dataloader = torch_data.DataLoader(dataset, **dataloader_kwargs)
+            dataloader_kwargs = {
+                'batch_size': sweep_cfg.batch_size,
+                'num_workers': 0 if cfg.DEBUG else cfg.DATALOADER.NUM_WORKER,
+                'shuffle': cfg.DATALOADER.SHUFFLE,
+                'drop_last': True,
+                'pin_memory': True,
+            }
+            dataloader = torch_data.DataLoader(dataset, **dataloader_kwargs)
 
-        # unpacking cfg
-        epochs = cfg.TRAINER.EPOCHS
-        steps_per_epoch = len(dataloader)
+            # unpacking cfg
+            epochs = cfg.TRAINER.EPOCHS
+            steps_per_epoch = len(dataloader)
 
-        # tracking variables
-        global_step = epoch_float = 0
+            # tracking variables
+            global_step = epoch_float = 0
 
-        # early stopping
-        best_f1_val = 0
-        trigger_times = 0
-        stop_training = False
+            # early stopping
+            best_f1_val = 0
+            trigger_times = 0
+            stop_training = False
 
-        for epoch in range(1, epochs + 1):
-            print(f'Starting epoch {epoch}/{epochs}.')
+            for epoch in range(1, epochs + 1):
+                print(f'Starting epoch {epoch}/{epochs}.')
 
-            start = timeit.default_timer()
-            loss_set = []
+                start = timeit.default_timer()
+                loss_set = []
 
-            for i, batch in enumerate(dataloader):
+                for i, batch in enumerate(dataloader):
 
-                net.train()
-                optimizer.zero_grad()
+                    net.train()
+                    optimizer.zero_grad()
 
-                x = batch['x'].to(device)
-                logits = net(x)
+                    x = batch['x'].to(device)
+                    logits = net(x)
 
-                y = batch['y'].to(device)
-                if cfg.MODEL.LOSS_TYPE == 'CrossEntropyLoss':
-                    logits = logits.flatten(start_dim=0, end_dim=1)
-                    y = y.flatten(start_dim=0, end_dim=1)[:, 0].long()
+                    y = batch['y'].to(device)
+                    if cfg.MODEL.LOSS_TYPE == 'CrossEntropyLoss':
+                        logits = logits.flatten(start_dim=0, end_dim=1)
+                        y = y.flatten(start_dim=0, end_dim=1)[:, 0].long()
 
-                loss = criterion(logits, y)
-                loss.backward()
-                optimizer.step()
+                    loss = criterion(logits, y)
+                    loss.backward()
+                    optimizer.step()
 
-                loss_set.append(loss.item())
+                    loss_set.append(loss.item())
 
-                global_step += 1
-                epoch_float = global_step / steps_per_epoch
+                    global_step += 1
+                    epoch_float = global_step / steps_per_epoch
 
-                if global_step % cfg.LOG_FREQ == 0:
-                    print(f'Logging step {global_step} (epoch {epoch_float:.2f}).')
+                    if global_step % cfg.LOG_FREQ == 0:
+                        print(f'Logging step {global_step} (epoch {epoch_float:.2f}).')
 
-                    # logging
-                    time = timeit.default_timer() - start
-                    wandb.log({
-                        'loss': np.mean(loss_set),
-                        'time': time,
-                        'step': global_step,
-                        'epoch': epoch_float,
-                    })
-                    start = timeit.default_timer()
-                    loss_set = []
-                # end of batch
+                        # logging
+                        time = timeit.default_timer() - start
+                        wandb.log({
+                            'loss': np.mean(loss_set),
+                            'time': time,
+                            'step': global_step,
+                            'epoch': epoch_float,
+                        })
+                        start = timeit.default_timer()
+                        loss_set = []
+                    # end of batch
 
-            assert (epoch == epoch_float)
-            print(f'epoch float {epoch_float} (step {global_step}) - epoch {epoch}')
-            # evaluation at the end of an epoch
-            _ = evaluation.model_evaluation(net, cfg, device, 'train', epoch_float, global_step)
-            f1_val = evaluation.model_evaluation(net, cfg, device, 'val', epoch_float, global_step)
+                assert (epoch == epoch_float)
+                print(f'epoch float {epoch_float} (step {global_step}) - epoch {epoch}')
+                # evaluation at the end of an epoch
+                _ = evaluation.model_evaluation(net, cfg, device, 'train', epoch_float, global_step)
+                f1_val = evaluation.model_evaluation(net, cfg, device, 'val', epoch_float, global_step)
 
-            if f1_val > best_f1_val:
-                best_f1_val = f1_val
-                print(f'saving network (F1 {f1_val:.3f})', flush=True)
-                model_factory.save_checkpoint(net, optimizer, epoch, cfg)
-                trigger_times = 0
-            else:
-                trigger_times += 1
-                if trigger_times >= cfg.TRAINER.PATIENCE:
-                    stop_training = True
+                if f1_val > best_f1_val:
+                    best_f1_val = f1_val
+                    print(f'saving network (F1 {f1_val:.3f})', flush=True)
+                    model_factory.save_checkpoint(net, optimizer, epoch, cfg)
+                    trigger_times = 0
+                else:
+                    trigger_times += 1
+                    if trigger_times >= cfg.TRAINER.PATIENCE:
+                        stop_training = True
 
-            if stop_training:
-                break
+                if stop_training:
+                    break
 
-        net, *_ = model_factory.load_checkpoint(cfg, device)
-        _ = evaluation.model_evaluation(net, cfg, device, 'test', epoch_float, global_step)
+            net, *_ = model_factory.load_checkpoint(cfg, device)
+            _ = evaluation.model_evaluation(net, cfg, device, 'test', epoch_float, global_step)
 
 
     if args.sweep_id is None:
