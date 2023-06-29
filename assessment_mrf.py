16a45ae0f24342b7cf9e66b7e188f795406f759c
SebastianHafner <hafnersailing@gmail.com>
2023-06-29 16:13:38 +0200

    final

--- assessment_mrf.py
+++ assessment_mrf.py
@@ -5,14 +5,12 @@ from pgmpy.models import MarkovNetwork
 from pgmpy.factors.discrete import DiscreteFactor
 from pgmpy.inference import BeliefPropagation
 
-import matplotlib.pyplot as plt
-import numpy as np
-from moviepy.editor import ImageSequenceClip
-from pathlib import Path
-
-from utils import datasets, parsers, experiment_manager, measurers
+from utils import datasets, parsers, experiment_manager, measurers, metrics, geofiles
 from utils.experiment_manager import CfgNode
 from models import model_factory
+
+from tqdm import tqdm
+from pathlib import Path
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -22,53 +20,65 @@ def mrf_unetouttransformer_v2(cfg: CfgNode, run_type: str = 'test'):
     net, *_ = model_factory.load_checkpoint(cfg, device)
     net.eval()
 
-    m_vanilla = measurers.MultiTaskLUNetMeasurer()
-    m_mrf = measurers.MeasurerMRF()
+    m_vanilla = measurers.MultiTaskMeasurer(name='vanilla')
+    m_mrf = measurers.MappingMeasurer(name='mrf')
 
-    ds = datasets.EvalDataset(cfg, run_type, tiling=cfg.AUGMENTATION.CROP_SIZE)
+    ds = datasets.EvalDataset(cfg, run_type, tiling=cfg.AUGMENTATION.CROP_SIZE, aoi_id='L15-0387E-1276N_1549_3087_13')
     dataloader = torch_data.DataLoader(ds, batch_size=1, num_workers=0, shuffle=False, drop_last=False)
 
-    for step, item in enumerate(dataloader):
-        x, y = item['x'].to(device), item['y'].to(device)
+    for step, item in enumerate(tqdm(dataloader)):
+        x, y_seg, y_ch = item['x'].to(device), item['y'].to(device), item['y_ch'].to(device)
         B, T, _, H, W = x.size()
 
         with torch.no_grad():
             logits_ch, logits_seg = net(x)
 
         y_hat_ch, y_hat_seg = torch.sigmoid(logits_ch).detach(), torch.sigmoid(logits_seg).detach()
-        m_vanilla.add_sample(y, y_hat_ch, y_hat_seg)
+        m_vanilla.add_sample(y_seg, y_hat_seg, y_ch, y_hat_ch)
 
-        for b in range(B):
-            for i in range(H):
-                for j in range(W):
-                    # Create a Markov Model (MRF)
-                    model = MarkovNetwork()
+        y_hat_seg_mrf = torch.empty(y_hat_seg.shape, dtype=torch.float32)
+        pixels = [(i, j) for i in range(H) for j in range(W)]
+        for i, j in pixels:
+            for b in range(B):
+                # Create a Markov Model (MRF)
+                model = MarkovNetwork()
 
-                    # Add nodes and node values
-                    for t in range(T):
-                        model.add_node(f'N{t}')
-                        # Values for node: P(urban=True), P(urban=False)
-                        urban_value = float(y_hat_seg[b, t, 0, i, j])
-                        factor = DiscreteFactor([f'N{t}'], cardinality=[2], values=[1 - urban_value, urban_value])
-                        model.add_factors(factor)
+                # Add nodes and node values
+                for t in range(T):
+                    model.add_node(f'N{t}')
+                    # Values for node: P(urban=True), P(urban=False)
+                    urban_value = float(y_hat_seg[b, t, 0, i, j])
+                    factor = DiscreteFactor([f'N{t}'], cardinality=[2], values=[1 - urban_value, urban_value])
+                    model.add_factors(factor)
 
-                    # add edges and edge values
-                    for t in range(T - 1):
-                        model.add_edge(f'N{t}', f'N{t+1}')
-                        change_value = float(y_hat_ch[b, t, 0, i, j])
-                        # [P(A=False, B=False), P(A=False, B=True), P(A=True, B=False), P(A=True, B=True)]
-                        factor = DiscreteFactor([f'N{t}', f'N{t+1}'], cardinality=[2, 2],
-                                                values=[1 - change_value, change_value, 0, 1 - change_value])
-                        model.add_factors(factor)
+                # add edges and edge values
+                for t in range(T - 1):
+                    model.add_edge(f'N{t}', f'N{t+1}')
+                    change_value = float(y_hat_ch[b, t, 0, i, j])
+                    # [P(A=False, B=False), P(A=False, B=True), P(A=True, B=False), P(A=True, B=True)]
+                    factor = DiscreteFactor([f'N{t}', f'N{t+1}'], cardinality=[2, 2],
+                                            values=[1 - change_value, change_value, 0, 1 - change_value])
+                    model.add_factors(factor)
 
+                # Create an instance of BeliefPropagation algorithm
+                bp = BeliefPropagation(model)
 
-                    # Create an instance of BeliefPropagation algorithm
-                    bp = BeliefPropagation(model)
+                # Compute the most probable state of the MRF
+                state = bp.map_query()
+                states_list = [state[f'N{t}'] for t in range(T)]
+                y_hat_seg_mrf[b, :, 0, i, j] = torch.Tensor(states_list)
 
-                    # Compute the most probable state of the MRF
-                    state = bp.map_query()
+        m_mrf.add_sample(y_seg.cpu(), y_hat_seg_mrf)
 
-                    print(state)
+    data = {}
+    for m in [m_vanilla, m_mrf]:
+        data[m.name] = {}
+        for attr in ['seg_cont', 'seg_fl', 'ch_cont', 'ch_fl']:
+            f1 = metrics.f1_score(getattr(m, f'TP_{attr}'), getattr(m, f'TP_{attr}'), getattr(m, f'TN_{attr}'))
+            data[m.name][attr] = f1.item()
+
+    out_file = Path(cfg.PATHS.OUTPUT) / 'assessment' / f'{cfg.NAME}.json'
+    geofiles.write_json(out_file, data)
 
 
 if __name__ == '__main__':
--- utils/measurers.py
+++ utils/measurers.py
@@ -1,11 +1,13 @@
+import abc
 import torch
 from utils import metrics
 
 
-class Measurer(object):
-    def __init__(self, threshold: float = 0.5):
+class AbstractMeasurer(abc.ABC):
+    def __init__(self, threshold: float = 0.5, name: str = None):
 
         self.threshold = threshold
+        self.name = name
 
         # urban mapping
         self.TP_seg_cont = self.TN_seg_cont = self.FP_seg_cont = self.FN_seg_cont = 0
@@ -16,54 +18,42 @@ class Measurer(object):
         self.TP_ch_fl = self.TN_ch_fl = self.FP_ch_fl = self.FN_ch_fl = 0
 
         # temporal consistency
-        self.unsup_tc_values = []
-        self.sup_tc_values = []
-        self.sup_tc_urban_values = []
+        self.unsup_tc_values, self.sup_tc_values, self.sup_tc_urban_values = [], [], []
 
-    def add_sample(self, y: torch.Tensor, y_hat: torch.Tensor):
-        B, T, _, H, W = y.size()
+    def add_sample(self, *args, **kwargs):
+        raise NotImplementedError("add_sample method must be implemented in the subclass.")
 
+    def _update_metrics(self, y: torch.Tensor, y_hat: torch.Tensor, attr_name: str):
         y = y.bool()
         y_hat = y_hat > self.threshold
 
-        # urban mapping
-        # continuous
-        self.TP_seg_cont += torch.sum(y & y_hat).float()
-        self.TN_seg_cont += torch.sum(~y & ~y_hat).float()
-        self.FP_seg_cont += torch.sum(y_hat & ~y).float()
-        self.FN_seg_cont += torch.sum(~y_hat & y).float()
-        # first last
-        self.TP_seg_fl += torch.sum(y[:, [0, -1]] & y_hat[:, [0, -1]]).float()
-        self.TN_seg_fl += torch.sum(~y[:, [0, -1]] & ~y_hat[:, [0, -1]]).float()
-        self.FP_seg_fl += torch.sum(y_hat[:, [0, -1]] & ~y[:, [0, -1]]).float()
-        self.FN_seg_fl += torch.sum(~y_hat[:, [0, -1]] & y[:, [0, -1]]).float()
+        tp_attr = f'TP_{attr_name}'
+        tn_attr = f'TN_{attr_name}'
+        fp_attr = f'FP_{attr_name}'
+        fn_attr = f'FN_{attr_name}'
 
-        # urban change
-        # continuous change
-        for t in range(1, T):
-            y_ch = ~torch.eq(y[:, t], y[:, t - 1])
-            y_hat_ch = ~torch.eq(y_hat[:, t], y_hat[:, t - 1])
-            self.TP_ch_cont += torch.sum(y_ch & y_hat_ch).float()
-            self.TN_ch_cont += torch.sum(~y_ch & ~y_hat_ch).float()
-            self.FP_ch_cont += torch.sum(y_hat_ch & ~y_ch).float()
-            self.FN_ch_cont += torch.sum(~y_hat_ch & y_ch).float()
-        # first last change
-        y_ch = ~torch.eq(y[:, -1], y[:, 0])
-        y_hat_ch = ~torch.eq(y_hat[:, -1], y_hat[:, 0])
-        self.TP_ch_fl += torch.sum(y_ch & y_hat_ch).float()
-        self.TN_ch_fl += torch.sum(~y_ch & ~y_hat_ch).float()
-        self.FP_ch_fl += torch.sum(y_hat_ch & ~y_ch).float()
-        self.FN_ch_fl += torch.sum(~y_hat_ch & y_ch).float()
+        setattr(self, tp_attr, getattr(self, tp_attr) + torch.sum(y & y_hat).float())
+        setattr(self, tn_attr, getattr(self, tn_attr) + torch.sum(~y & ~y_hat).float())
+        setattr(self, fp_attr, getattr(self, fp_attr) + torch.sum(y_hat & ~y).float())
+        setattr(self, fn_attr, getattr(self, fn_attr) + torch.sum(~y_hat & y).float())
 
-        # temporal consistency
-        for b in range(B):
-            self.unsup_tc_values.append(metrics.unsupervised_tc(y_hat[b]))
-            self.sup_tc_values.append(metrics.supervised_tc(y[b], y_hat[b]))
-            self.sup_tc_urban_values.append(metrics.supervised_tc_urban(y[b], y_hat[b]))
+    def _update_temporal_consistency(self, y_seg: torch.Tensor, y_hat_seg: torch.Tensor):
+        y_seg = y_seg.bool()
+        y_hat_seg = y_hat_seg > self.threshold
+        for b in range(y_hat_seg.size(0)):
+            self.unsup_tc_values.append(metrics.unsupervised_tc(y_hat_seg[b]))
+            self.sup_tc_values.append(metrics.supervised_tc(y_seg[b], y_hat_seg[b]))
+            self.sup_tc_urban_values.append(metrics.supervised_tc_urban(y_seg[b], y_hat_seg[b]))
 
-    def is_empty(self):
-        assert (len(self.unsup_tc_values) == len(self.sup_tc_values))
-        return True if len(self.sup_tc_values) == 0 else False
+    @staticmethod
+    def _cont_ch_from_seg(y_seg: torch.Tensor, y_hat_seg: torch.Tensor) -> tuple:
+        B, T, C, H, W = y_seg.size()
+        y_ch = torch.empty((B, T - 1, C, H, W), dtype=torch.bool)
+        y_hat_ch = torch.empty((B, T - 1, C, H, W), dtype=torch.bool)
+        for t in range(T - 1):
+            y_ch[:, t] = torch.ne(y_seg[:, t + 1], y_seg[:, t])
+            y_hat_ch[:, t] = torch.ne(y_hat_seg[:, t + 1], y_hat_seg[:, t])
+        return y_ch, y_hat_ch
 
     def reset(self):
         # urban mapping
@@ -79,231 +69,81 @@ class Measurer(object):
         self.sup_tc_values = []
         self.sup_tc_urban_values = []
 
+    # def is_empty(self):
+    #     raise NotImplementedError("is_empty method must be implemented in the subclass.")
 
-class ChangeMeasurer(object):
-    def __init__(self, threshold: float = 0.5):
 
-        self.threshold = threshold
+class MappingMeasurer(AbstractMeasurer):
+    def __init__(self, threshold: float = 0.5, name: str = None):
+        super().__init__(threshold, name)
 
-        # urban change | cch -> continuous change | flch -> first to last change
-        self.TP_ch_cont = self.TN_ch_cont = self.FP_ch_cont = self.FN_ch_cont = 0
-        self.TP_ch_fl = self.TN_ch_fl = self.FP_ch_fl = self.FN_ch_fl = 0
+    def add_sample(self, y_seg: torch.Tensor, y_hat_seg: torch.Tensor):
+
+        # urban mapping
+        self._update_metrics(y_seg, y_hat_seg, 'seg_cont')
+        self._update_metrics(y_seg[:, [0, -1]], y_hat_seg[:, 0, -1], 'seg_fl')
+
+        # urban change
+        y_ch_cont, y_hat_ch_cont = self._cont_ch_from_seg(y_seg, y_hat_seg)
+        self._update_metrics(y_ch_cont, y_hat_ch_cont, 'ch_cont')
+
+        y_ch_fl = torch.ne(y_seg[:, -1], y_seg[:, 0])
+        y_hat_ch_fl = torch.ne(y_hat_seg[:, -1], y_hat_seg[:, 0])
+        self._update_metrics(y_ch_fl, y_hat_ch_fl, 'ch_fl')
+
+        self._update_temporal_consistency(y_seg, y_hat_seg)
+
+
+class ChangeMeasurer(AbstractMeasurer):
+    def __init__(self, threshold: float = 0.5, name: str = None):
+        super().__init__(threshold, name)
 
     def add_sample(self, y_ch: torch.Tensor, y_hat_ch: torch.Tensor, change_method: str):
-        B, T, _, H, W = y_ch.size()
-
-        y_ch = y_ch.bool()
-        y_hat_ch = y_hat_ch > self.threshold
 
         if change_method == 'bitemporal':
             # continuous change
-            self.TP_ch_cont += torch.sum(y_ch & y_hat_ch).float()
-            self.TN_ch_cont += torch.sum(~y_ch & ~y_hat_ch).float()
-            self.FP_ch_cont += torch.sum(y_hat_ch & ~y_ch).float()
-            self.FN_ch_cont += torch.sum(~y_hat_ch & y_ch).float()
+            self._update_metrics(y_ch, y_hat_ch, 'ch_cont')
 
             # first last change derived from continuous change
-            y_ch = torch.sum(y_ch, dim=1) > 0
-            y_hat_ch = torch.sum(y_hat_ch, dim=1) > 0
-            self.TP_ch_fl += torch.sum(y_ch & y_hat_ch).float()
-            self.TN_ch_fl += torch.sum(~y_ch & ~y_hat_ch).float()
-            self.FP_ch_fl += torch.sum(y_hat_ch & ~y_ch).float()
-            self.FN_ch_fl += torch.sum(~y_hat_ch & y_ch).float()
+            y_ch_fl = torch.sum(y_ch, dim=1) > 0
+            y_hat_ch_fl = torch.sum(y_hat_ch, dim=1) > 0
+            self._update_metrics(y_ch_fl, y_hat_ch_fl, 'ch_fl')
+
         elif change_method == 'timeseries':
-            y_ch = torch.sum(y_ch, dim=1) > 0
-            self.TP_ch_fl += torch.sum(y_ch & y_hat_ch).float()
-            self.TN_ch_fl += torch.sum(~y_ch & ~y_hat_ch).float()
-            self.FP_ch_fl += torch.sum(y_hat_ch & ~y_ch).float()
-            self.FN_ch_fl += torch.sum(~y_hat_ch & y_ch).float()
+            y_ch_fl = torch.sum(y_ch, dim=1) > 0
+            self._update_metrics(y_ch_fl, y_hat_ch, 'ch_fl')
+
         else:
             raise Exception('Unknown change method!')
 
-    def reset(self):
-        # urban change | cch -> continuous change | flch -> first to last change
-        self.TP_ch_cont = self.TN_ch_cont = self.FP_ch_cont = self.FN_ch_cont = 0
-        self.TP_ch_fl = self.TN_ch_fl = self.FP_ch_fl = self.FN_ch_fl = 0
 
+class MultiTaskMeasurerLimited(AbstractMeasurer):
+    def __init__(self, threshold: float = 0.5, name: str = None):
+        super().__init__(threshold, name)
 
-class MultiTaskLUNetMeasurer(object):
-    def __init__(self, threshold: float = 0.5):
-
-        self.threshold = threshold
-
-        # urban mapping
-        self.TP_seg_fl = self.TN_seg_fl = self.FP_seg_fl = self.FN_seg_fl = 0
-
-        # urban change | flch -> first to last change
-        self.TP_ch_fl = self.TN_ch_fl = self.FP_ch_fl = self.FN_ch_fl = 0
-
-    def add_sample(self, y: torch.Tensor, y_hat_ch: torch.Tensor, y_hat_seg: torch.Tensor):
-        B, T, _, H, W = y.size()
-
-        y = y.bool()
-        y_hat_ch = y_hat_ch > self.threshold
-        y_hat_seg = y_hat_seg > self.threshold
+    def add_sample(self, y_seg: torch.Tensor, y_hat_seg: torch.Tensor, y_hat_ch_fl: torch.Tensor):
 
         # urban mapping first last
-        self.TP_seg_fl += torch.sum(y[:, [0, -1]] & y_hat_seg[:, [0, -1]]).float()
-        self.TN_seg_fl += torch.sum(~y[:, [0, -1]] & ~y_hat_seg[:, [0, -1]]).float()
-        self.FP_seg_fl += torch.sum(y_hat_seg[:, [0, -1]] & ~y[:, [0, -1]]).float()
-        self.FN_seg_fl += torch.sum(~y_hat_seg[:, [0, -1]] & y[:, [0, -1]]).float()
+        self._update_metrics(y_seg[:, [0, -1]], y_hat_seg[:, 0, -1], 'seg_fl')
 
         # urban change first last change
-        y_ch = torch.ne(y[:, -1], y[:, 0])
-        self.TP_ch_fl += torch.sum(y_ch & y_hat_ch).float()
-        self.TN_ch_fl += torch.sum(~y_ch & ~y_hat_ch).float()
-        self.FP_ch_fl += torch.sum(y_hat_ch & ~y_ch).float()
-        self.FN_ch_fl += torch.sum(~y_hat_ch & y_ch).float()
-
-    def is_empty(self):
-        return True if self.TP_seg_fl == 0 else False
-
-    def reset(self):
-        # urban mapping
-        self.TP_seg_fl = self.TN_seg_fl = self.FP_seg_fl = self.FN_seg_fl = 0
-
-        # urban change | flch -> first to last change
-        self.TP_ch_fl = self.TN_ch_fl = self.FP_ch_fl = self.FN_ch_fl = 0
+        y_ch_fl = torch.ne(y_seg[:, -1], y_seg[:, 0])
+        self._update_metrics(y_ch_fl, y_hat_ch_fl, 'ch_fl')
 
 
-class MeasurerProposed(object):
-    def __init__(self, threshold: float = 0.5):
-
-        self.threshold = threshold
-
-        # urban mapping
-        self.TP_seg_cont = self.TN_seg_cont = self.FP_seg_cont = self.FN_seg_cont = 0
-        self.TP_seg_fl = self.TN_seg_fl = self.FP_seg_fl = self.FN_seg_fl = 0
-
-        # urban change | cch -> continuous change | flch -> first to last change
-        self.TP_ch_cont = self.TN_ch_cont = self.FP_ch_cont = self.FN_ch_cont = 0
-        self.TP_ch_fl = self.TN_ch_fl = self.FP_ch_fl = self.FN_ch_fl = 0
-
-        # temporal consistency
-        self.unsup_tc_values = []
-        self.sup_tc_values = []
-        self.sup_tc_urban_values = []
+class MultiTaskMeasurer(AbstractMeasurer):
+    def __init__(self, threshold: float = 0.5, name: str = None):
+        super().__init__(threshold, name)
 
     def add_sample(self, y_seg: torch.Tensor, y_hat_seg: torch.Tensor, y_ch: torch.Tensor, y_hat_ch: torch.Tensor):
-        B, T, _, H, W = y_seg.size()
-
-        y_seg, y_ch = y_seg.bool(), y_ch.bool()
-        y_hat_seg, y_hat_ch = y_hat_seg > self.threshold, y_hat_ch > self.threshold
 
         # urban mapping
-        # continuous
-        self.TP_seg_cont += torch.sum(y_seg & y_hat_seg).float()
-        self.TN_seg_cont += torch.sum(~y_seg & ~y_hat_seg).float()
-        self.FP_seg_cont += torch.sum(y_hat_seg & ~y_seg).float()
-        self.FN_seg_cont += torch.sum(~y_hat_seg & y_seg).float()
-        # first last
-        self.TP_seg_cont += torch.sum(y_seg[:, [0, -1]] & y_hat_seg[:, [0, -1]]).float()
-        self.TN_seg_cont += torch.sum(~y_seg[:, [0, -1]] & ~y_hat_seg[:, [0, -1]]).float()
-        self.FP_seg_cont += torch.sum(y_hat_seg[:, [0, -1]] & ~y_seg[:, [0, -1]]).float()
-        self.FN_seg_cont += torch.sum(~y_hat_seg[:, [0, -1]] & y_seg[:, [0, -1]]).float()
+        self._update_metrics(y_seg, y_hat_seg, 'seg_cont')
+        self._update_metrics(y_seg[:, [0, -1]], y_hat_seg[:, 0, -1], 'seg_fl')
 
         # urban change
-        # continuous change
-        self.TP_ch_cont += torch.sum(y_ch[:, :-1] & y_hat_ch[:, :-1]).float()
-        self.TN_ch_cont += torch.sum(~y_ch[:, :-1] & ~y_hat_ch[:, :-1]).float()
-        self.FP_ch_cont += torch.sum(y_hat_ch[:, :-1] & ~y_ch[:, :-1]).float()
-        self.FN_ch_cont += torch.sum(~y_hat_ch[:, :-1] & y_ch[:, :-1]).float()
-        # first last change
-        self.TP_ch_fl += torch.sum(y_ch[:, -1] & y_hat_ch[:, -1]).float()
-        self.TN_ch_fl += torch.sum(~y_ch[:, -1] & ~y_hat_ch[:, -1]).float()
-        self.FP_ch_fl += torch.sum(y_hat_ch[:, -1] & ~y_ch[:, -1]).float()
-        self.FN_ch_fl += torch.sum(~y_hat_ch[:, -1] & y_ch[:, -1]).float()
+        self._update_metrics(y_ch[:, :-1], y_hat_ch[:, :-1], 'ch_cont')
+        self._update_metrics(y_ch[:, -1], y_hat_ch[:, -1], 'ch_fl')
 
         # temporal consistency
-        for b in range(B):
-            self.unsup_tc_values.append(metrics.unsupervised_tc(y_hat_seg[b]))
-            self.sup_tc_values.append(metrics.supervised_tc(y_seg[b], y_hat_seg[b]))
-            self.sup_tc_urban_values.append(metrics.supervised_tc_urban(y_seg[b], y_hat_seg[b]))
-
-    def is_empty(self):
-        assert (len(self.unsup_tc_values) == len(self.sup_tc_values))
-        return True if len(self.sup_tc_values) == 0 else False
-
-    def reset(self):
-        # urban mapping
-        self.TP_seg_cont = self.TN_seg_cont = self.FP_seg_cont = self.FN_seg_cont = 0
-        self.TP_seg_fl = self.TN_seg_fl = self.FP_seg_fl = self.FN_seg_fl = 0
-
-        # urban change | cch -> continuous change | flch -> first to last change
-        self.TP_ch_cont = self.TN_ch_cont = self.FP_ch_cont = self.FN_ch_cont = 0
-        self.TP_ch_fl = self.TN_ch_fl = self.FP_ch_fl = self.FN_ch_fl = 0
-
-        # temporal consistency
-        self.unsup_tc_values = []
-        self.sup_tc_values = []
-        self.sup_tc_urban_values = []
-
-
-class MeasurerMRF(object):
-    def __init__(self):
-
-        # urban mapping
-        self.TP_seg_cont = self.TN_seg_cont = self.FP_seg_cont = self.FN_seg_cont = 0
-        self.TP_seg_fl = self.TN_seg_fl = self.FP_seg_fl = self.FN_seg_fl = 0
-
-        # urban change | cch -> continuous change | flch -> first to last change
-        self.TP_ch_cont = self.TN_ch_cont = self.FP_ch_cont = self.FN_ch_cont = 0
-        self.TP_ch_fl = self.TN_ch_fl = self.FP_ch_fl = self.FN_ch_fl = 0
-
-        # temporal consistency
-        self.unsup_tc_values = []
-        self.sup_tc_values = []
-        self.sup_tc_urban_values = []
-
-    def add_sample(self, y_seg: torch.Tensor, y_hat_seg: torch.Tensor):
-        B, T, _, H, W = y_seg.size()
-
-        y_seg, y_hat_seg = y_seg.bool(), y_hat_seg.bool()
-
-        # urban mapping
-        # continuous
-        self.TP_seg_cont += torch.sum(y_seg & y_hat_seg).float()
-        self.TN_seg_cont += torch.sum(~y_seg & ~y_hat_seg).float()
-        self.FP_seg_cont += torch.sum(y_hat_seg & ~y_seg).float()
-        self.FN_seg_cont += torch.sum(~y_hat_seg & y_seg).float()
-        # first last
-        self.TP_seg_cont += torch.sum(y_seg[:, [0, -1]] & y_hat_seg[:, [0, -1]]).float()
-        self.TN_seg_cont += torch.sum(~y_seg[:, [0, -1]] & ~y_hat_seg[:, [0, -1]]).float()
-        self.FP_seg_cont += torch.sum(y_hat_seg[:, [0, -1]] & ~y_seg[:, [0, -1]]).float()
-        self.FN_seg_cont += torch.sum(~y_hat_seg[:, [0, -1]] & y_seg[:, [0, -1]]).float()
-
-        # urban change
-        # continuous change
-        self.TP_ch_cont += torch.sum(y_ch[:, :-1] & y_hat_ch[:, :-1]).float()
-        self.TN_ch_cont += torch.sum(~y_ch[:, :-1] & ~y_hat_ch[:, :-1]).float()
-        self.FP_ch_cont += torch.sum(y_hat_ch[:, :-1] & ~y_ch[:, :-1]).float()
-        self.FN_ch_cont += torch.sum(~y_hat_ch[:, :-1] & y_ch[:, :-1]).float()
-        # first last change
-        self.TP_ch_fl += torch.sum(y_ch[:, -1] & y_hat_ch[:, -1]).float()
-        self.TN_ch_fl += torch.sum(~y_ch[:, -1] & ~y_hat_ch[:, -1]).float()
-        self.FP_ch_fl += torch.sum(y_hat_ch[:, -1] & ~y_ch[:, -1]).float()
-        self.FN_ch_fl += torch.sum(~y_hat_ch[:, -1] & y_ch[:, -1]).float()
-
-        # temporal consistency
-        for b in range(B):
-            self.unsup_tc_values.append(metrics.unsupervised_tc(y_hat_seg[b]))
-            self.sup_tc_values.append(metrics.supervised_tc(y_seg[b], y_hat_seg[b]))
-            self.sup_tc_urban_values.append(metrics.supervised_tc_urban(y_seg[b], y_hat_seg[b]))
-
-    def is_empty(self):
-        assert (len(self.unsup_tc_values) == len(self.sup_tc_values))
-        return True if len(self.sup_tc_values) == 0 else False
-
-    def reset(self):
-        # urban mapping
-        self.TP_seg_cont = self.TN_seg_cont = self.FP_seg_cont = self.FN_seg_cont = 0
-        self.TP_seg_fl = self.TN_seg_fl = self.FP_seg_fl = self.FN_seg_fl = 0
-
-        # urban change | cch -> continuous change | flch -> first to last change
-        self.TP_ch_cont = self.TN_ch_cont = self.FP_ch_cont = self.FN_ch_cont = 0
-        self.TP_ch_fl = self.TN_ch_fl = self.FP_ch_fl = self.FN_ch_fl = 0
-
-        # temporal consistency
-        self.unsup_tc_values = []
-        self.sup_tc_values = []
-        self.sup_tc_urban_values = []
+        self._update_temporal_consistency(y_seg, y_hat_seg)
